877148c50401c778ab55a0e748f633e546c790ec
Arman Avasthi <armanavasthi@github.com>
2018-05-12 15:22:23 +0530

    committed code before introducing jwt

--- src/main/java/com/arman/armaNote/config/SecurityConfig.java
+++ src/main/java/com/arman/armaNote/config/SecurityConfig.java
@@ -161,5 +161,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	return bCryptPasswordEncoder;
     }
     
+    // to implement jwt, go through https://medium.com/@nydiarra/secure-a-spring-boot-rest-api-with-json-web-token-reference-to-angular-integration-e57a25806c50
+    
 
 }
